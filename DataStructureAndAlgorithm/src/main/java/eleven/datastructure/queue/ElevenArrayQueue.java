6cd077a17412bbbef012768f5aef751a11a2fee9
eleven <10993105@qq.com>
2020-03-17 17:57:00 +0800

    update

--- DataStructureAndAlgorithm/src/main/java/eleven/datastructure/queue/ElevenArrayQueue.java
+++ DataStructureAndAlgorithm/src/main/java/eleven/datastructure/queue/ElevenArrayQueue.java
@@ -4,21 +4,24 @@ import java.lang.reflect.Array;
 import java.util.*;
 
 /**
- * 队列顺序存储结构 基于数组实现队列
- *
- * 不足：单是顺序存储，出列每次所有元素向前移动，性能较低
- * 循环队列，数组会溢出
+ * 循环队列
  */
 public class ElevenArrayQueue<E> {
     final Object[] items;
 
-    /** 队头下标 循环队列的关键*/
-    int takeIndex;
+    /**
+     * 队头下标
+     */
+    int front;
 
-    /** 队尾下边*/
-    int putIndex;
+    /**
+     * 队尾下标
+     */
+    int rear;
 
-    /** 元素计数 */
+    /**
+     * 元素计数
+     */
     int count;
 
     public ElevenArrayQueue(int capacity) {
@@ -29,15 +32,41 @@ public class ElevenArrayQueue<E> {
         return count;
     }
 
-    public boolean offer(E e) {
-        return false;
+    public void enqueue(E e) {
+        if(count == items.length){
+            throw new IllegalMonitorStateException("queue already full");
+        }
+        items[rear] = e;
+        if (++rear == items.length) rear = 0;//数组末尾
+        count++;
     }
 
-    public E poll() {
-        return null;
+    public E dequeue() {
+        E e = (E) items[front];
+        items[front] = null;
+        if (++front == items.length) front = 0;//数组末尾
+        count--;
+        return e;
     }
 
-    public E peek() {
-        return null;
+    public static void main(String[] args) {
+        ElevenArrayQueue<Integer> queue = new ElevenArrayQueue<Integer>(10);
+        for (int i = 0; i < 10; i++) {
+            queue.enqueue(i);
+            System.out.println(Arrays.toString(queue.items));
+        }
+        for (int i = 0; i < 5; i++) {
+            queue.dequeue();
+            System.out.println(Arrays.toString(queue.items));
+        }
+        for (int i = 0; i < 4; i++) {
+            queue.enqueue(i);
+            System.out.println(Arrays.toString(queue.items));
+        }
+        for (int i = 0; i < 2; i++) {
+            queue.enqueue(i);
+            System.out.println(Arrays.toString(queue.items));
+        }
+//        System.out.println(Arrays.toString(queue.items));
     }
 }
