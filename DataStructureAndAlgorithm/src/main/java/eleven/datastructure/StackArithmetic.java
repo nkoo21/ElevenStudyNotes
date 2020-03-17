6535cba24ec2d1f53f3bd824b38fac6978db675c
eleven <10993105@qq.com>
2020-03-17 14:13:30 +0800

    init

---
+++ DataStructureAndAlgorithm/src/main/java/eleven/datastructure/StackArithmetic.java
@@ -0,0 +1,13 @@
+package eleven.datastructure;
+
+import java.util.Stack;
+
+public class StackArithmetic {
+
+    public void suffix(String expression){
+
+    }
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<Integer>();
+    }
+}
